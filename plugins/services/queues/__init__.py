6d6c95e3872de69954380a307fd8412a45f6404c
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-25 10:40:43 +0530

    Update __init__.py

--- plugins/services/queues/__init__.py
+++ plugins/services/queues/__init__.py
@@ -1,7 +1,7 @@
-from VCPlayBot.services.queues.queues import clear 
-from VCPlayBot.services.queues.queues import get
-from VCPlayBot.services.queues.queues import is_empty
-from VCPlayBot.services.queues.queues import put
-from VCPlayBot.services.queues.queues import task_done
+from plugins.services.queues.queues import clear 
+from plugins.services.queues.queues import get
+from plugins.services.queues.queues import is_empty
+from plugins.services.queues.queues import put
+from plugins.services.queues.queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
